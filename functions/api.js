ebd6cb8f9ef3d93aeaf0968335c56ed7f9fa77ba
William Baele <baelewilliam1999@gmail.com>
2023-03-13 16:21:54 +0100

    Finished

--- functions/api.js
+++ functions/api.js
@@ -1,3 +1,8 @@
-function fetchJSON (url, options = {}) {
-  const headers = {options}
+export async function fetchJSON (url, options = {}) {
+  const headers = {Acccept: 'application/json', ...options.headers}
+  const r = await fetch(url, {...options, headers})
+  if(r.ok) {
+    return r.json()
+  }
+  throw new Error('Impossible de contacter le serveur', {cause: r})
 }
